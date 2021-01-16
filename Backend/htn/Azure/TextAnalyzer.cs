a8d67d284d37ce85e71e66e8755d05f1b829e934
encodeous <encodeous7@gmail.com>
2021-01-16 10:51:55 -0500

    stuff

---
+++ Backend/htn/Azure/TextAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using Azure;
+using Azure.AI.TextAnalytics;
+
+namespace htn.Azure
+{
+    public class TextAnalyzer
+    {
+        private static Uri endpoint = new Uri("https://htn-analyzer.cognitiveservices.azure.com/");
+        private TextAnalyticsClient Client;
+        public TextAnalyzer()
+        {
+            var credentials = new AzureKeyCredential(File.ReadAllText("Secret.txt"));
+            Client = new TextAnalyticsClient(endpoint, credentials);
+        }
+
+        public float GetSentiment(string s)
+        {
+            DocumentSentiment documentSentiment = Client.AnalyzeSentiment(s);
+            float score = 0;
+
+            foreach (var sentence in documentSentiment.Sentences)
+            {
+                if (sentence.Sentiment == TextSentiment.Negative) score--;
+                if (sentence.Sentiment == TextSentiment.Mixed) score-=0.5f;
+                if (sentence.Sentiment == TextSentiment.Positive) score++;
+            }
+
+            return score / documentSentiment.Sentences.Count;
+        }
+    }
+}
--- Backend/htn/Startup.cs
+++ Backend/htn/Startup.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using htn.Azure;
 using htn.ML;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
@@ -29,6 +30,7 @@ namespace htn
             services.AddRazorPages();
             services.AddServerSideBlazor();
             services.AddSingleton<PyBridge>();
+            services.AddSingleton<TextAnalyzer>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
